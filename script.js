d8fbca0d25f66056540e7d0c7f66bca3459fdfc4
GrumpyArdias <this.mario@gmail.com>
2023-02-16 15:56:26 +0100

    Added form validation

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-// Progress BAR
 const progressbar = document.querySelector(".progress-bar");
 const bodyElement = document.querySelector("html");
+const toTopBtn = document.querySelector(".to-top");
+const toTopAnchor = document.getElementById("anchor-to-top");
+const burguerIcon = document.getElementById("burguer-img");
+const NavMenu = document.getElementById("nav-menu");
+const nameInput = document.getElementById("name");
+const mail = document.getElementById("email");
+const form = document.getElementById("contact-form");
+const checkbox = document.getElementById("terms-of-serice");
+const errorDiv = document.getElementById("error-msg");
+// Progress BAR
 
 const animateProgressBar = () => {
   let scrollDistance = -bodyElement.getBoundingClientRect().top;
@@ -17,10 +26,6 @@ const animateProgressBar = () => {
 window.addEventListener("scroll", animateProgressBar);
 
 // To top button
-
-const toTopBtn = document.querySelector(".to-top");
-const toTopAnchor = document.getElementById("anchor-to-top");
-
 toTopBtn.addEventListener("click", function () {
   setTimeout(function () {
     toTopAnchor.hrref = "#header";
@@ -28,12 +33,36 @@ toTopBtn.addEventListener("click", function () {
 });
 
 // burguer menu
-
-const burguerIcon = document.getElementById("burguer-img");
-const NavMenu = document.getElementById("nav-menu");
-
 burguerIcon.addEventListener("click", () => {
   NavMenu.style.display === "none"
     ? (NavMenu.style.display = "flex")
     : (NavMenu.style.display = "none");
 });
+
+// Input validation
+
+form.addEventListener("submit", (e) => {
+  let errorMsg = [];
+
+  let mailRex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  if (nameInput.value.length < 2 || nameInput.value.length >= 100) {
+    errorMsg.push(
+      "El nombre no puede ser menor de 2 caracteres ni mayor de 100"
+    );
+    nameInput.style.borderColor = "#fc0303";
+  }
+
+  if (!mail.value.match(mailRex)) {
+    errorMsg.push("El mail introducido no es valido");
+    mail.style.borderColor = "#fc0303";
+  }
+
+  if (errorMsg.length > 0) {
+    e.preventDefault();
+    errorDiv.innerHTML = errorMsg.join(", ");
+    errorDiv.style.color = "#fc0303";
+  }
+});
+//El nombre tiene que tener entre 2 y 100 letras,
