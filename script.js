ad307d2c5e5a84f5fd710caa342cdf75365287f0
GrumpyArdias <this.mario@gmail.com>
2023-02-20 12:27:34 +0100

    Refactor and modal

--- script.js
+++ script.js
@@ -4,26 +4,95 @@ const toTopBtn = document.querySelector(".to-top");
 const toTopAnchor = document.getElementById("anchor-to-top");
 const burguerIcon = document.getElementById("burguer-img");
 const NavMenu = document.getElementById("nav-menu");
-const nameInput = document.getElementById("name");
-const mail = document.getElementById("email");
-const form = document.getElementById("contact-form");
-const checkbox = document.getElementById("terms-of-serice");
+const formContact = document.getElementById("contact-form");
 const errorDiv = document.getElementById("error-msg");
-// Progress BAR
+const nchlBtn = document.getElementById("nwsl-btn");
+const closeBtn = document.getElementById("close-Btn");
 
-const animateProgressBar = () => {
+const formData = {
+  userName: document.getElementById("name"),
+  userMail: document.getElementById("email"),
+  //checkbox: document.getElementById("terms-of-serice"),
+  nchMail: document.getElementById("subscribe"),
+};
+
+function progressCalculations() {
   let scrollDistance = -bodyElement.getBoundingClientRect().top;
   let progressWidth =
     (scrollDistance /
       (bodyElement.getBoundingClientRect().height -
         document.documentElement.clientHeight)) *
     100;
-  let progress = Math.ceil(progressWidth);
+  return Math.ceil(progressWidth);
+}
 
-  progressbar.style.width = progress + "%";
+function dataValidations(event) {
+  let errorMsg = [];
+
+  let mailRex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  console.log(formData);
+  if (formData.userName.length < 2 || formData.userName.length >= 100) {
+    errorMsg.push(
+      "El nombre no puede ser menor de 2 caracteres ni mayor de 100"
+    );
+    formData.userName.style.borderColor = "#fc0303";
+  }
+
+  if (!formData.userMail.value.match(mailRex)) {
+    errorMsg.push("El mail introducido no es valido");
+    formData.userMail.style.borderColor = "#fc0303";
+  }
+
+  if (
+    !formData.nchMail.value.match(mailRex || formData.nchMail.value !== null)
+  ) {
+    formData.nchMail.style.borderColor = "#fc0303";
+  }
+
+  if (errorMsg.length > 0) {
+    event.preventDefault();
+    errorDiv.innerHTML = errorMsg.join(", ");
+    errorDiv.style.color = "#fc0303";
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function fetchHandel(name, mail, nlMail) {
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title: "OxygenShop Contact Request",
+      UserName: name,
+      mail: mail,
+      nchMail: nlMail,
+      userid: Date.now(),
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json))
+    .catch((e) => console.error(e));
+}
+
+function closeModal() {
+  document.querySelector(".modal").style.display = "none";
+  document.body.style.backgroundColor = "white";
+}
+// Progress BAR
+const animateProgressBar = (porcentage) => {
+  progressbar.style.width = porcentage + "%";
 };
 
-window.addEventListener("scroll", animateProgressBar);
+window.addEventListener("scroll", () => {
+  const progress = progressCalculations();
+  animateProgressBar(progress);
+});
 
 // To top button
 toTopBtn.addEventListener("click", function () {
@@ -39,46 +108,65 @@ burguerIcon.addEventListener("click", () => {
     : (NavMenu.style.display = "none");
 });
 
-// Input validation and Fetch data
+// MODAL
 
-form.addEventListener("submit", (e) => {
-  let errorMsg = [];
+let modalHasPop = false;
 
-  let mailRex =
-    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+window.onload = function () {
+  console.log(`el estado del token es ${modalHasPop}`);
+  if (!modalHasPop) {
+    setTimeout(() => {
+      document.getElementById("modal").style.display = "flex";
+      document.body.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+      modalHasPop = true;
+      console.log(
+        `el estado del token despues de las funciones es ${modalHasPop}`
+      );
+    }, 5000);
 
-  if (nameInput.value.length < 2 || nameInput.value.length >= 100) {
-    errorMsg.push(
-      "El nombre no puede ser menor de 2 caracteres ni mayor de 100"
-    );
-    nameInput.style.borderColor = "#fc0303";
+    window.onscroll = () => {
+      if (
+        !modalHasPop &&
+        window.pageYOffset > document.documentElement.scrollHeight * 0.25
+      ) {
+        document.getElementById("modal").style.display = "flex";
+        document.body.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+        modalHasPop = true;
+        console.log(
+          `el estado del token despues de las funciones es ${modalHasPop}`
+        );
+      }
+    };
   }
+};
 
-  if (!mail.value.match(mailRex)) {
-    errorMsg.push("El mail introducido no es valido");
-    mail.style.borderColor = "#fc0303";
-  }
+// event listener
 
-  if (errorMsg.length > 0) {
+formContact.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  console.log(dataValidations());
+
+  if (dataValidations()) {
     e.preventDefault();
-    errorDiv.innerHTML = errorMsg.join(", ");
-    errorDiv.style.color = "#fc0303";
-  } else {
-    e.preventDefault();
-    fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "POST",
-      body: JSON.stringify({
-        title: "OxygenShop Contact Request",
-        UserName: nameInput,
-        mail: mail.value,
-        userId: Date.now(),
-      }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    })
-      .then((response) => response.json())
-      .then((json) => console.log(json))
-      .catch((e) => console.log("error"));
+    fetchHandel(formData.userName.value, formData.userMail.value, null);
   }
 });
+
+document.addEventListener(
+  "click",
+  function (event) {
+    if (event.target.matches(".close-Btn") || !event.target.closest(".modal")) {
+      closeModal();
+    }
+  },
+  false
+);
+
+closeBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.getElementById("modal").style.display = "none";
+  document.body.style.backgroundColor = "white";
+});
+
+// TO DO SCROLL ALWAYS POPS
